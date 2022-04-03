6bc48d278fc16039f13a5ae60c3e3c5e88f7964b
sachinkarun <69583217+sachinkarun@users.noreply.github.com>
2022-04-03 13:40:56 +0530

    Add files via upload

---
+++ front-end/redux/reducers/index.js
@@ -0,0 +1,11 @@
+//rootReducer
+import { combineReducers } from "redux";
+import { user } from './user';
+import { users } from './users';
+
+const Reducers = combineReducers({
+    userState: user,
+    usersState: users
+})
+
+export default Reducers;
